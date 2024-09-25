29726d8e54d0206b9e9c53f4ffef89a19ec6e021
alumno <alumno@pc-czarate.uacj.mx>
2024-09-25 10:07:08 -0600

    Nuevos cambios en las funciones

--- boceto_1/boceto_1/ViewController.swift
+++ boceto_1/boceto_1/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     var cita_para_enviar: Cita = Cita(quien_lo_dijo: "Cuadrado Básico", que_dijo: "Guácala con mi vida")
+    var citas_disponibles: GeneradorDeCitas = GeneradorDeCitas()
 
     override func viewDidLoad() {
+        citas_disponibles.generar_citas_falsas()
         super.viewDidLoad()
         // Do any additional setup after loading the view
+        let valor = Int.random(in: 0...100)
+        Double.random(in: 0...100)
     }
 
 
