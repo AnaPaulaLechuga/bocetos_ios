5298348493bbf87a90b3cf916ca21b6a0ab036f3
alumno <alumno@pc-czarate.uacj.mx>
2024-10-11 10:43:33 -0600

    Agregar nuevas clases y métodos

---
+++ boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/estilos/EstiloCellViewControllerCelular.swift
@@ -0,0 +1,69 @@
+//
+//  EstiloCellViewControllerCelular.swift
+//  boceto_2_CellView
+//
+//  Created by alumno on 10/11/24.
+//
+import UIKit
+
+extension ControladorPantallaPrincipalDeColeccion: UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var ancho = self.collectionView.frame.width
+        var largo = self.collectionView.frame.height
+                
+        ancho = ancho / 3
+        largo = ancho * 0.5
+                
+        return CGSize(width: ancho, height: largo)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout:
+                        UICollectionViewLayout, insetForSectionAt sction: Int) -> UIEdgeInsets {
+        var margin = CGFloat(25)
+        return UIEdgeInsets(top: margin, left: margin, bottom: margin, right: margin)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        
+        return 500
+    }
+            // Method 2
+            /*func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+                
+                return 5
+            }
+            // Method 3
+            func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+                
+                return 5
+            }
+            //Method 4
+            func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+                
+                let collectionViewWidth = self.collectionView.frame.width
+                let collectionViewHeight =  self.collectionView.frame.height
+                
+                let cellWidth = (collectionViewWidth) / 1.1
+                let cellHeight = cellWidth * 0.5
+                
+                return CGSize(width: cellWidth , height: cellHeight)
+                
+            }
+            
+            override func viewWillLayoutSubviews() {
+                print("REPRENDER ESTAS mmadas")
+                
+            }
+            
+            override func viewWillAppear(_ animated: Bool) {
+                (self.navigationController as? mod_navegador_principal)?.activar_navigation_bar(actviar: false)
+
+            }
+        }
+    }*/
+}
--- boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/vista_de_celdas/ControladorPantallaPrincipalDeColeccion.swift
+++ boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/vista_de_celdas/ControladorPantallaPrincipalDeColeccion.swift
@@ -11,33 +11,46 @@ import UIKit
 class ControladorPantallaPrincipalDeColeccion: UICollectionViewController {
     
     private var lista_de_publicaciones: [Publicacion] = []
-    
+    let url_de_publicaciones = "https://jsonplaceholder.typicode.com/posts"
+
+        
     private let identificador_de_celda = "celda_pantalla_principal"
+
+    let proveedor_publicaciones = ProveedorDePublicaciones.autoreferencia
+        
+    @IBOutlet weak var outlet_a_la_vista: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        /*
          let ubicacion = URL(string: url_de_publicaciones)!
-         URLSession.shared.dataTask(with: ubicacion) {(datos, respuesta, error)
-         in do {
-         if let publicaciones_recibidas = datos{
-         let prueba_de_interpretacion_de_datos = try JSONDecoder().decode([Publicacion].self, from: publicaciones_recibidas){
-         
-         }
-         }
-         else {
-         print(respuesta)
-         }
-         }
-         catch {
-         print("Error")
-         }
+         URLSession.shared.dataTask(with: ubicacion) {
+             (datos, respuesta, error) in do {
+                 if let publicaciones_recibidas = datos{
+                     let prueba_de_interpretacion_de_datos = try JSONDecoder().decode([Publicacion].self, from: publicaciones_recibidas)
+                     
+                     self.lista_de_publicaciones = prueba_de_interpretacion_de_datos
+                     DispatchQueue.main.async {
+                         self.collectionView.reloadData()
+                     }
+                 }
+                 else {
+                     print(respuesta)
+                 }
+             }
+            catch {
+                print("Error")
+            }
          } .resume()
          
-         print(lista_de_publicaciones)
-         */
-        ProveedorDePublicaciones().obtener_publicaciones(que_hacer_al_recibir: { (lista_de_publicaciones) in self.lista_de_publicaciones = lista_de_publicaciones}
-                                                         
+        /*
+         proveedor_publicaciones.obtener_publicaicones{
+            [weak self] (publicaciones) in self?.lista_de_publicaciones = publicaciones
+            DispatchQueue.main.async {
+            elf?.collectionView.reloadData()
+            }
+        }
+        */
     }
 
 
@@ -55,13 +68,13 @@ class ControladorPantallaPrincipalDeColeccion: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 2
+        return 1
     }
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return 5+5
+        return self.lista_de_publicaciones.count
     }
 
     //Función para identificar y crear cada una de las celdas creadas en el Controller
@@ -71,13 +84,20 @@ class ControladorPantallaPrincipalDeColeccion: UICollectionViewController {
         // Configure the cell
         //celda.backgroundColor = UIColor.blue
         
-        celda.etiqueta.text = "\(indexPath)"
+        celda.etiqueta.text = self.lista_de_publicaciones[indexPath.item].title
+        celda.cuerpo.text = self.lista_de_publicaciones[indexPath.item].body
         
         return celda
     }
     
     override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("Se seleccionó la celda \(indexPath)")
+        
+        let pantalla_de_publicacion = storyboard?.instantiateViewController(withIdentifier: "PantallaPublicacion") as! ControladorPantallaDelPost
+                
+        self.navigationController?.pushViewController(pantalla_de_publicacion, animated: true)
+                
+        print(self.navigationController)
     }
 
     // MARK: UICollectionViewDelegate
@@ -109,6 +129,46 @@ class ControladorPantallaPrincipalDeColeccion: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, performAction action: Selector, forItemAt indexPath: IndexPath, withSender sender: Any?) {
     
     }
+     
+     override func numberOfSections(in collectionView: UICollectionView) -> Int {
+             // #warning Incomplete implementation, return the number of sections
+             return 1
+         }
+
+
+         override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+             // #warning Incomplete implementation, return the number of items
+             return self.lista_de_publicaciones.count
+         }
+
+         // Funcion para identificar y crear cada una de las celdas creadas en el Controller
+         override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+             let celda: VistaDeZelda = collectionView.dequeueReusableCell(withReuseIdentifier: identificador_de_celda, for: indexPath) as! VistaDeZelda
+         
+             // Configure the cell
+             //celda.tintColor = UIColor.green
+             
+             celda.etiqueta.text = self.lista_de_publicaciones[indexPath.item].title
+             celda.cuerpo.text = self.lista_de_publicaciones[indexPath.item].body
+
+             // print(self.lista_de_publicaciones)
+             
+             return celda
+         }
+         
+
+         
+         override func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+             print("Se selecciono la celda\(indexPath)")
+             
+             let pantalla_de_publicacion = storyboard?.instantiateViewController(withIdentifier: "PantallaPublicacion") as! ControladorPantallaDelPost
+             
+             self.navigationController?.pushViewController(pantalla_de_publicacion, animated: true)
+             
+             print(self.navigationController)
+
+         }
+
     */
 
 }
