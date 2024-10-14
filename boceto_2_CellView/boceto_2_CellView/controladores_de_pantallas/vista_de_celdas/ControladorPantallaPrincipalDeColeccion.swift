f06c8f514d18e695f6b078655b79b5ef3d999815
alumno <alumno@pc-czarate.uacj.mx>
2024-10-14 09:56:04 -0600

    Poner más cambios en el diseño por si a caso

--- boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/vista_de_celdas/ControladorPantallaPrincipalDeColeccion.swift
+++ boceto_2_CellView/boceto_2_CellView/controladores_de_pantallas/vista_de_celdas/ControladorPantallaPrincipalDeColeccion.swift
@@ -68,7 +68,7 @@ class ControladorPantallaPrincipalDeColeccion: UICollectionViewController {
 
     override func numberOfSections(in collectionView: UICollectionView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 10
+        return 1
     }
 
 
