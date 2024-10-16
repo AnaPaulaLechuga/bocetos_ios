f1cd9193cd2c59f98fee3c5b4b3474483c237055
alumno <alumno@pc-czarate.uacj.mx>
2024-10-16 10:40:48 -0600

    Corrección de errores de previsualización

--- aplicacion_prueba/aplicacion_prueba/ContentView.swift
+++ aplicacion_prueba/aplicacion_prueba/ContentView.swift
@@ -10,30 +10,26 @@ import RealityKit
 import RealityKitContent
 
 struct ContentView: View {
-    @State var pulsaciones: Int = 0
+    @State var pulsaciones: Bool = true
     var body: some View {
         HStack{
             VStack {
-                Model3D(named: "Scene", bundle: realityKitContentBundle)
-                    .padding(.bottom, 50)
-                Model3D(named: "Sun", bundle: realityKitContentBundle)
-                    .padding(.bottom, 0)
+                if pulsaciones{
+                    Model3D(named: "Scene", bundle: realityKitContentBundle)
+                        .padding(.bottom, 50)
+                }
+                else{
+                    Model3D(named: "Sun", bundle: realityKitContentBundle)
+                        .padding(.bottom, 0)
+                }
             }
             Text("Hello, world!")
             Text("Hello, worawld!\(pulsaciones)")
             
-            switch pulsaciones{
-                case 1:
-                    Text("Halo")
-                case 2:
-                    Text("Hali 3")
-                default:
-                    Text("Default")
-            }
             
             Text("Hello, world!")
             Button(action: {
-                pulsaciones += 1
+                pulsaciones = !pulsaciones
                 print("Hola mundo desde consola\(pulsaciones)")},
                    label: {Text("Hola mundo")})
         }
